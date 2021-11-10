e2a0ce7b8a82369aa2569b6c38e0a436fe7ee6e1
xenia <xenia.bshkv@gmail.com>
2021-11-10 22:28:21 +0700

    Добавьте файлы проекта.

---
+++ PZ_1/PZ_1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void firstAssigment() {
+    int numbers[3];
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    double sum = 0.;
+
+    cout << "Please type 3 numbers using space as separator" << endl;
+    cin >> numbers[0] >> numbers[1] >> numbers[2];
+
+    for (auto number : numbers) {
+        sum += number;
+    }
+
+    size = sizeof(numbers) / sizeof(numbers[0]);
+
+    cout << "Average of three numbers is " << sum / size << endl;
+}
+
+int fahrenheitOffset() {
+    return 32;
+}
+
+double fahrenheitCoefficient() {
+    return 9 / 5.;
+}
+
+void thirdAssigment() {
+    double celsius = 0., fahrenheit = 0.;
+
+    cout << "Please enter the temperature in Celsius" << endl;
+    cin >> celsius;
+
+    fahrenheit = (celsius * fahrenheitCoefficient()) + fahrenheitOffset();
+
+    cout << "Temperature in Fahrenheit is " << fahrenheit << endl;
+}
+
+int main()
+{
+    firstAssigment();
+
+    system("clear");
+
+    thirdAssigment();
+}
